6c73178fb90b6596311a17a6160762abe6bca5bc
PaoloTolentino <tolentinopaolomatthew23@gmail.com>
2024-01-26 14:50:08 +0800

    feat: updated prisma schema to use supabase

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,27 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from './strategies/local.strategy';
 import { AuthController } from './auth.controller';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
-import { JwtStrategy } from './strategies/jwt.strategy';
+import { SupabaseSrategy } from './strategies/supabase.strategy';
 
 @Module({
-  imports: [
-    UserModule,
-    PassportModule,
-    JwtModule.registerAsync({
-      useFactory: async (configService: ConfigService) => {
-        return {
-          secret: configService.get<string>('JWT_SECRET'),
-          signOptions: { expiresIn: '1d' },
-        };
-      },
-      inject: [ConfigService],
-    }),
-  ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  imports: [UserModule, PassportModule],
+  providers: [AuthService, SupabaseSrategy],
   controllers: [AuthController],
   exports: [AuthService],
 })
---
+++ src/auth/strategies/supabase.strategy.ts
@@ -0,0 +1,27 @@
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { SupabaseAuthStrategy } from 'nestjs-supabase-auth';
+import { ExtractJwt } from 'passport-jwt';
+
+export class SupabaseSrategy extends PassportStrategy(
+  SupabaseAuthStrategy,
+  'supabase',
+) {
+  constructor(configService: ConfigService) {
+    super({
+      supabaseKey: configService.get('SUPABASE_KEY') ?? '',
+      supabaseUrl: configService.get('SUPABASE_URL') ?? '',
+      supabaseOptions: {},
+      supabaseJwtSecret: configService.get('SUPABASE_JWT_SECRET'),
+      extractor: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: any): Promise<any> {
+    super.validate(payload);
+  }
+
+  authenticate(req: any) {
+    super.authenticate(req);
+  }
+}
